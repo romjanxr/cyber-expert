2728802feff0d6296e1759dfedfc2ebcef76d8c8
romjanxr <romjanvr5@gmail.com>
2021-09-26 02:34:44 +0600

    item and cart complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import CartElement from '../CartElement/CartElement';
 import './Cart.css'
 
 const Cart = props => {
     const { cart } = props;
     const total = cart.reduce((previous, expert) => previous + expert.salary, 0);
-    const img = cart.map(expert => expert.name)
+    const addedExpert = cart.map(expert => {
+        return (
+            <CartElement expert={expert}></CartElement>
+        )
+    })
     return (
-        <div classNameName="cart-bg">
+        <div className="cart-bg card">
             <h4>Expert Added: {cart.length}</h4>
             <h4>Total Cost: {total}</h4>
-            <p>{img}</p>
+            <div>{addedExpert}</div>
+            {
+                !cart.length || <button className="btn btn-primary">Confirm List</button>
+            }
         </div>
     );
 };
--- src/components/CartElement/CartElement.js
+++ src/components/CartElement/CartElement.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-const CartElement = () => {
+const CartElement = props => {
+    const { img, name } = props.expert;
     return (
-        <div>
-            <div className="card mb-3" style={{ maxWidth: '540px' }}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img width="200px" src="..." className="img-fluid rounded-start" alt="..." />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
+        <div className="card my-3 element-bg">
+            <div className="row g-0">
+                <div className="col-md-3">
+                    <img src={img} className="img-fluid rounded-start img-left" alt="..." />
+                </div>
+                <div className="col-md-9">
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
                     </div>
                 </div>
             </div>
